10734aa19ea388fdc7ae6483ca25715d9ca17537
mercy-brice-kamilindi <mercy.brice.kamilindi@stu.ocsb.ca>
2025-10-23 13:24:15 -0400

    grabs distance and turns on neopxiel

--- main.ts
+++ main.ts
@@ -1,8 +1,46 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by:Mercy
+ * Created on: Oct 2025
+ * This program grabs distance and turns on neopixels
 */
 
-basic.showString('Hello, World!')
+// variable
+let distanceToObject: number = 0
+let neopixelStrip: neopixel.Strip = null
+
+// Clean
+basic.clearScreen()
+neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.show()
+basic.showIcon(IconNames.Happy)
+
+
+input.onButtonPressed(Button.A, function() {
+    distanceToObject = sonar.ping(
+        DigitalPin.P8,
+        DigitalPin.P12,
+        PingUnit.Centimeters)
+        basic.showNumber(distanceToObject)
+    
+    // turn light on
+    if (distanceToObject < 10) {
+    neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
+    neopixelStrip.show()
+      }  else {
+    neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Green))
+    neopixelStrip.show()
+      }
+})
