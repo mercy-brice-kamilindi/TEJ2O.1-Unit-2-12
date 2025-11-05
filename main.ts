adde07b216f2d68ae925dbede073bffd452ad7d3
mercy-brice-kamilindi <mercy.brice.kamilindi@stu.ocsb.ca>
2025-11-05 13:06:52 -0500

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -5,29 +5,29 @@
  * This program grabs distance and turns on neopixels
 */
 
-// variable
-let distanceToObject: number = 0
-let neopixelStrip: neopixel.Strip = null
+        // variable
+        let distanceToObject: number = 0
+        let neopixelStrip: neopixel.Strip = null
 
-// Clean
-   basic.clearScreen()
-   neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
-   neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
-   neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
-   neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
-   neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
-   neopixelStrip.show()
-   basic.showIcon(IconNames.Happy)
+        // Clean
+        basic.clearScreen()
+        neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.show()
+        basic.showIcon(IconNames.Happy)
 
 
-input.onButtonPressed(Button.A, function() {
-    distanceToObject = sonar.ping(
+        input.onButtonPressed(Button.A, function() {
+        distanceToObject = sonar.ping(
         DigitalPin.P8,
         DigitalPin.P12,
         PingUnit.Centimeters)
         basic.showNumber(distanceToObject)
     
-    // turn light on
+        // turn light on
         if (distanceToObject < 10) {
         neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
         neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
